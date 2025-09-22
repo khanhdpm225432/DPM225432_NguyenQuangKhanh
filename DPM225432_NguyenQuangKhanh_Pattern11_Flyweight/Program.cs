83289d0a840736f6efaf82a2bd174a816d8fe0ed
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-09-22 10:34:14 +0700

    Add files via upload

---
+++ DPM225432_NguyenQuangKhanh_Pattern11_Flyweight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DPM225458_PhamHuuPhuoc_Pattern11_Flyweight
+
+{
+    /// <summary>
+    /// Flyweight Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Arbitrary extrinsic state
+            int extrinsicstate = 22;
+            FlyweightFactory factory = new FlyweightFactory();
+            // Work with different flyweight instances
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+            UnsharedConcreteFlyweight fu = new
+                UnsharedConcreteFlyweight();
+            fu.Operation(--extrinsicstate);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
