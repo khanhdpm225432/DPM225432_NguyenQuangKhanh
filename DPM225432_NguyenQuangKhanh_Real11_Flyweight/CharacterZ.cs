c1e30640ed6751ec7644a4d490d339db09bb3267
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-09-22 10:35:14 +0700

    Add files via upload

---
+++ DPM225432_NguyenQuangKhanh_Real11_Flyweight/CharacterZ.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225458_PhamHuuPhuoc_Real11_Flyweight
+{
+    public class CharacterZ : Character
+    {
+        // Constructor
+        public CharacterZ()
+        {
+            symbol = 'Z';
+            height = 100;
+            width = 100;
+            ascent = 68;
+            descent = 0;
+        }
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol +
+                " (pointsize " + this.pointSize + ")");
+        }
+    }
+}
