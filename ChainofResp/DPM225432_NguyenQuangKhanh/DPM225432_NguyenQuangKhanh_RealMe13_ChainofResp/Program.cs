22cae487396c6d73bf369e1138ee9f3f2361dc37
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-10-06 09:18:38 +0700

    Add files via upload

---
+++ ChainofResp/DPM225432_NguyenQuangKhanh/DPM225432_NguyenQuangKhanh_RealMe13_ChainofResp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225512_VoKhoaNguyen_RealMe13_ChainofResp
+{
+    using System;
+
+    namespace ChainDemo
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                NguoiDuyet truongPhong = new TruongPhong();
+                NguoiDuyet phoGiamDoc = new PhoGiamDoc();
+                NguoiDuyet giamDoc = new GiamDoc();
+
+                truongPhong.ThietLapNguoiKeTiep(phoGiamDoc);
+                phoGiamDoc.ThietLapNguoiKeTiep(giamDoc);
+
+                YeuCauMuaHang yc1 = new YeuCauMuaHang(2034, 350.00, "Văn phòng phẩm");
+                truongPhong.XuLyYeuCau(yc1);
+
+                YeuCauMuaHang yc2 = new YeuCauMuaHang(2035, 32590.10, "Dự án X");
+                truongPhong.XuLyYeuCau(yc2);
+
+                YeuCauMuaHang yc3 = new YeuCauMuaHang(2036, 122100.00, "Dự án Y");
+                truongPhong.XuLyYeuCau(yc3);
+
+                Console.ReadKey();
+            }
+        }
+
+        public abstract class NguoiDuyet
+        {
+            protected NguoiDuyet keTiep;
+
+            public void ThietLapNguoiKeTiep(NguoiDuyet keTiep)
+            {
+                this.keTiep = keTiep;
+            }
+
+            public abstract void XuLyYeuCau(YeuCauMuaHang yeuCau);
+        }
+
+        public class TruongPhong : NguoiDuyet
+        {
+            public override void XuLyYeuCau(YeuCauMuaHang yeuCau)
+            {
+                if (yeuCau.SoTien < 10000.0)
+                {
+                    Console.WriteLine("Trưởng phòng duyệt yêu cầu #{0}", yeuCau.MaSo);
+                }
+                else if (keTiep != null)
+                {
+                    keTiep.XuLyYeuCau(yeuCau);
+                }
+            }
+        }
+
+        public class PhoGiamDoc : NguoiDuyet
+        {
+            public override void XuLyYeuCau(YeuCauMuaHang yeuCau)
+            {
+                if (yeuCau.SoTien < 25000.0)
+                {
+                    Console.WriteLine("Phó giám đốc duyệt yêu cầu #{0}", yeuCau.MaSo);
+                }
+                else if (keTiep != null)
+                {
+                    keTiep.XuLyYeuCau(yeuCau);
+                }
+            }
+        }
+
+        public class GiamDoc : NguoiDuyet
+        {
+            public override void XuLyYeuCau(YeuCauMuaHang yeuCau)
+            {
+                if (yeuCau.SoTien < 100000.0)
+                {
+                    Console.WriteLine("Giám đốc duyệt yêu cầu #{0}", yeuCau.MaSo);
+                }
+                else
+                {
+                    Console.WriteLine("Yêu cầu #{0} cần họp ban giám đốc!", yeuCau.MaSo);
+                }
+            }
+        }
+
+        public class YeuCauMuaHang
+        {
+            public int MaSo { get; set; }
+            public double SoTien { get; set; }
+            public string LyDo { get; set; }
+
+            public YeuCauMuaHang(int maSo, double soTien, string lyDo)
+            {
+                MaSo = maSo;
+                SoTien = soTien;
+                LyDo = lyDo;
+            }
+        }
+    }
+}
---
+++ ChainofResp/DPM225432_NguyenQuangKhanh/DPM225432_NguyenQuangKhanh_RealWorld13_ChainofResp/ConcreteHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225512_VoKhoaNguyen_RealWorld13_ChainofResp
+{
+    public class Director : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 10000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+    /// <summary>
+    /// The 'ConcreteHandler' class
+    /// </summary>
+    public class VicePresident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 25000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+    /// <summary>
+    /// The 'ConcreteHandler' class
+    /// </summary>
+    public class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine(
+                    "Request# {0} requires an executive meeting!",
+                    purchase.Number);
+            }
+        }
+    }
+    /// <summary>
+    /// Class holding request details
+    /// </summary>
+    public class Purchase
+    {
+        int number;
+        double amount;
+        string purpose;
+        // Constructor
+        public Purchase(int number, double amount, string purpose)
+        {
+            this.number = number;
+            this.amount = amount;
+            this.purpose = purpose;
+        }
+        // Gets or sets purchase number
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        // Gets or sets purchase amount
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        // Gets or sets purchase purpose
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+    }
+}
