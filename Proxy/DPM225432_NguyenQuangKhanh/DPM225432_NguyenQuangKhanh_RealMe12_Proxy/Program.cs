eca05c1a18f236291607341ed6f4a71f9b6a1f13
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-10-06 09:19:29 +0700

    Add files via upload

---
+++ Proxy/DPM225432_NguyenQuangKhanh/DPM225432_NguyenQuangKhanh_RealMe12_Proxy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProxyDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MayTinhProxy mayTinh = new MayTinhProxy();
+
+            Console.WriteLine("4 + 2 = " + mayTinh.Cong(4, 4));
+            Console.WriteLine("4 - 2 = " + mayTinh.Tru(4, 4));
+            Console.WriteLine("4 * 2 = " + mayTinh.Nhan(4, 4));
+            Console.WriteLine("4 / 2 = " + mayTinh.Chia(4, 4));
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IMayTinh
+    {
+        double Cong(double a, double b);
+        double Tru(double a, double b);
+        double Nhan(double a, double b);
+        double Chia(double a, double b);
+    }
+
+    public class MayTinh : IMayTinh
+    {
+        public double Cong(double a, double b) { return a + b; }
+        public double Tru(double a, double b) { return a - b; }
+        public double Nhan(double a, double b) { return a * b; }
+        public double Chia(double a, double b) { return a / b; }
+    }
+
+    public class MayTinhProxy : IMayTinh
+    {
+        private MayTinh mayTinh = new MayTinh();
+
+        public double Cong(double a, double b) { return mayTinh.Cong(a, b); }
+        public double Tru(double a, double b) { return mayTinh.Tru(a, b); }
+        public double Nhan(double a, double b) { return mayTinh.Nhan(a, b); }
+        public double Chia(double a, double b) { return mayTinh.Chia(a, b); }
+    }
+}
---
+++ Proxy/DPM225432_NguyenQuangKhanh/DPM225432_NguyenQuangKhanh_RealWorld12_Proxy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225512_VoKhoaNguyen_RealWorld12_Proxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create math proxy
+            MathProxy proxy = new MathProxy();
+            // Do the math
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Subject interface
+    /// </summary>
+    public interface IMath
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mul(double x, double y);
+        double Div(double x, double y);
+    }
+}
