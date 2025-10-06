fa6e1a267abf2208284d5911282e9d5b60a8b0dd
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-10-06 09:26:58 +0700

    Add files via upload

---
+++ Mediator/DPM225432_NguyenQuangKhanh/DPM225432_NguyenQuangKhanh_RealWorld17_Mediator/ConcreteColleague.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225512_VoKhoaNguyen_RealWorld17_Mediator
+{
+    public class Beatle : Participant
+    {
+        // Constructor
+        public Beatle(string name)
+            : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+    /// <summary>
+    /// A 'ConcreteColleague' class
+    /// </summary>
+    public class NonBeatle : Participant
+    {
+        // Constructor
+        public NonBeatle(string name)
+            : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a non-Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+}
+
