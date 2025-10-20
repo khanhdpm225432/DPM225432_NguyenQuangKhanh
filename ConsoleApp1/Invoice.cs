ee17804ef11de238d1f815da6b0cbd56720356f7
khanhdpm225432 <khanh_dpm225432@student.agu.edu.vn>
2025-10-20 08:43:26 +0700

    Add files via upload

---
+++ ConsoleApp1/Invoice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extract_Method
+{
+  
+
+    public class Invoice
+    {
+        private string name;
+        private double outstanding;
+
+        public Invoice(string name, double outstanding)
+        {
+            this.name = name;
+            this.outstanding = outstanding;
+        }
+
+        public void PrintOwing()
+        {
+            PrintBanner();
+            PrintDetails();
+        }
+
+        private void PrintBanner()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("**** Customer Owes ****");
+            Console.WriteLine("***********************");
+        }
+
+        private void PrintDetails()
+        {
+            Console.WriteLine("Name: " + (name ?? "Unknown"));
+            Console.WriteLine("Amount: " + outstanding.ToString("F2"));
+        }
+
+        public double GetOutstanding()
+        {
+            return outstanding;
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Extract_Method;
+using System.Xml.Linq;
+
+namespace Extract_Method
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice("Nguyen Van A", 1234.56);
+            invoice.PrintOwing();
+        }
+    }
+
+}
